800e049741dfa3698e79d5ddca1d49c3caeb372b
kirioka <hei.amour.2@gmail.com>
2022-06-02 15:53:06 +0300

    update readme

---
+++ insert/villes.sql
@@ -0,0 +1,5 @@
+insert into ville (nom_ville) values ('Antananarivo'), 
+                        ('Toliara'),('Fianarantsoa'),
+                        ('Toamasina'), ('Antsirabe'), 
+                        ('Brickaville'), ('Mahajanga'),
+                        ('Tsiroanomandidy'),('Diego'); 
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Insert for our Database
+
+| tables    | number of values |
+| --------- | ---------------- |
+| chauffeur |               10 |
+| client    |              300 |
+| vehicule  |               15 |
+| ville     |                9 |
+
