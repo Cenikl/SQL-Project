797203b245aec457e6c273b6d9f029ab46254b00
Loïca <95331041+NiceGuyMe@users.noreply.github.com>
2022-06-16 15:24:14 +0300

    Create afficher_toutes_les_reservation_sur_un_voyage.sql

---
+++ requête/afficher_toutes_les_reservation_sur_un_voyage.sql
@@ -0,0 +1,3 @@
+//toutes les reservations sur le voyage 3
+
+select id_voyage,nom,prenom,date_reservation,place,montant_paye from reserver inner join client on reserver.id_client=client.id_client where id_voyage=3;
