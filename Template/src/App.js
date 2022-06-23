2dc852af374ccea793b4093895f76ff857093eea
Cedric <99885260+Cenikl@users.noreply.github.com>
2022-06-23 12:17:02 +0300

    template react

---
+++ Template/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App(){
+return (<div>
+<header class="nav" id="mynav">
+<a href="#home" class="home">Sign in</a>
+<a href="#home" class="home">News</a>
+  <a href="./index.html" class="home">Home</a>
+  <a href="./index.html" class="trans">Toky Trans</a>
+</header>
+
+<div>
+</div>
+
+<footer>
+<div class="footer-gray">
+  <div class="footer-custom">
+      <h3>2021-2022</h3>
+      <p>@Created by an authentic team :D</p>
+    </div>
+</div>
+</footer>
+</div>
+)
+}
+
+export default App;
